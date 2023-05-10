1b68c622fc0a5f4de7d3e0155435cf9a47e75db9
TJ Stifter <tjstifter@gmail.com>
2023-05-10 13:37:06 -0400

    add wonky stuff

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const h2 = document.createElement('h2');
 h2.textContent = "Hello... my Name is...";
 
 document.querySelector('body').appendChild(h2);
+
+const div2 = document.createElement('div');
+div2.textContent = "I was added in by a Script :)"
+document.querySelector('body').appendChild(div2);
+
+const input = document.getElementById('button');
+input.addEventListener('click', function(){
+    alert('Hi');
+});
